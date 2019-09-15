0934c12beec62c4bfcb10569ba698fc5fc48acee
Jari Karjala <jka@iki.fi>
2019-09-15 12:34:35 +0300

    Added fasthttp client

--- gomb-fasthttp/fasthttp.go
+++ gomb-fasthttp/fasthttp.go
@@ -1,49 +1,41 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/jkarjala/gomb"
+	"github.com/valyala/fasthttp"
 )
 
 // Long options for the testers, short ones used by the main library
 var httpAuth = flag.String("auth", "", "HTTP Authorization header")
 var httpContentType = flag.String("content-type", "application/json", "HTTP body content type")
-var httpTimeout = flag.Int("timeout", 10, "HTTP Client timeout")
 
 func main() {
-	log.Println("HTTP tester started")
+	log.Println("FastHTTP tester started")
 	gomb.Run(clientFactory)
 }
 
 type myClient struct {
 	id         int
 	template   *gomb.VarTemplate
-	httpClient *http.Client
+	req        *fasthttp.Request
+	res        *fasthttp.Response
+	httpClient *fasthttp.Client
 }
 
 func clientFactory(id int, template string) (gomb.Client, error) {
-	log.Println(id, "http init", template)
+	log.Println(id, "fasthttp init", template)
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-		MaxIdleConnsPerHost: *gomb.NumClients,
-		DisableCompression:  false,
-		DisableKeepAlives:   false,
-	}
-	tr.TLSNextProto = make(map[string]func(string, *tls.Conn) http.RoundTripper)
-	httpClient := &http.Client{Transport: tr, Timeout: time.Duration(*httpTimeout) * time.Second}
-	var client = myClient{id, gomb.Parse(template), httpClient}
+	var req = fasthttp.AcquireRequest()
+	var res = fasthttp.AcquireResponse()
+	var httpClient = &fasthttp.Client{}
+	var client = myClient{id, gomb.Parse(template), req, res, httpClient}
 	return &client, nil
 }
 
@@ -53,8 +45,6 @@ func (c *myClient) RunCommand(in *gomb.RunInput) *gomb.RunResult {
 		cmd = c.template.Expand(in.Args)
 	}
 
-	var req *http.Request
-	var resp *http.Response
 	var err error
 
 	var ss = strings.SplitN(cmd, " ", 3)
@@ -68,42 +58,38 @@ func (c *myClient) RunCommand(in *gomb.RunInput) *gomb.RunResult {
 		body = strings.Trim(ss[2], "\r\t")
 	}
 
-	req, err = http.NewRequest(primitive, url, strings.NewReader(body))
-	if err != nil {
-		return &gomb.RunResult{Err: err}
-	}
+	c.req.Reset()
+	c.req.Header.SetMethod(primitive)
+	c.req.SetRequestURI(url)
 
 	if body != "" {
-		req.Header.Add("Content-Type", *httpContentType)
+		c.req.Header.Add("Content-Type", *httpContentType)
+		c.req.SetBody([]byte(body))
 	}
 	if *httpAuth != "" {
-		req.Header.Add("Authorization", *httpAuth)
+		c.req.Header.Add("Authorization", *httpAuth)
 	}
 
+	c.res.Reset()
 	var start = time.Now()
-	resp, err = c.httpClient.Do(req)
+	err = c.httpClient.Do(c.req, c.res)
 	elapsed := time.Since(start).Seconds()
 	if err != nil {
 		return &gomb.RunResult{Err: err, Time: elapsed}
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		body, _ := ioutil.ReadAll(resp.Body)
-		log.Printf("%d HTTP status %d body '%s'", c.id, resp.StatusCode, string(body))
-	} else {
-		// body, _ := ioutil.ReadAll(resp.Body)
-		// log.Printf("%s: %s\n", cmd, body)
-		io.Copy(ioutil.Discard, resp.Body)
+	status := c.res.StatusCode()
+	resBody := c.res.Body()
+	if status < 200 || status > 299 {
+		log.Printf("%d fasthttp status %d body '%s'", c.id, status, string(resBody))
 	}
 	elapsed = time.Since(start).Seconds() // final time will include body read time
-	var res = resp.Status
+	var res = fmt.Sprintf("%d %s", status, http.StatusText(status))
 	if *gomb.Verbose {
-		log.Printf("%d http %s: %s", c.id, cmd, res)
+		log.Printf("%d fasthttp %s '%s' body '%s'", c.id, cmd, res, string(resBody))
 	}
 	return &gomb.RunResult{Res: res, Time: elapsed}
 }
 
 func (c *myClient) Term() {
-	log.Println(c.id, "http term")
+	log.Println(c.id, "fasthttp term")
 }
